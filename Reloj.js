5d7ce20413dde6e7a640bf819891207ddc2a0c7f
DavidRenes <drdavidrenes@gmail.com>
2022-11-14 21:03:40 +0100

    Solo reloj

--- Reloj.js
+++ Reloj.js
@@ -5,28 +5,29 @@ function abrirVentana() {
       //Declaramos todas las propiedades de la ventana
       var propiedades = "width=300, height=200, top=100, left=200, resizable=no, scrollbars=no, location=no";
 
-
       //Abrimos la ventana
       miVentana = window.open("reloj.html", "", propiedades);
 
-      //Ponemos el foco en la ventana
+}
+
+//Ponemos el foco en la ventana
+function actualizar() {
       var fecha = new Date();
       var hor = fecha.getHours();
       var min = fecha.getMinutes();
       var seg = fecha.getSeconds();
 
-      document.getElementById("horas").innerHTML = hor.toLocaleString;
-      document.getElementById("minutos").innerHTML = min.toLocaleString;
-      document.getElementById("segundos").innerHTML = seg.toLocaleString;
-
-
+      document.getElementById("horas").innerHTML = hor;
+      document.getElementById("minutos").innerHTML = min;
+      document.getElementById("segundos").innerHTML = seg;
 }
+
 
 //Funcion para cerrar la ventana
 function cerrarVentana() {
-
       window.close();
-
 }
 
+//Al cargar la web se pone en marcha el reloj
+window.onload = actualizar;
 
